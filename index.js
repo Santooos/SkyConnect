b55685c9d9ba847207b4e75e9ddf577d140f88d1
Santosh Lamichhane <sant43210@gmail.com>
2023-11-04 12:44:51 -0400

    updates

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
+//const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require('./routes');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
 app.use('/', routes);
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
