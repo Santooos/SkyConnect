12f551bf942434ec7e53e3472c31f07cdcfee610
Santosh Lamichhane <sant43210@gmail.com>
2023-11-04 11:47:34 -0400

    setting up basic requirements

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const routes = require('./routes');
+app.use('/', routes);
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
 
 app.get('/', (req, res) => {
   res.send('SkyConnect Backend is running.');
