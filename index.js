51cd04a40b1a4bb5abd43eeb6cecc9241b5846a6
Santosh Lamichhane <sant43210@gmail.com>
2023-11-05 01:13:58 -0400

    updating with socket.io

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
+// index.js
 require('dotenv').config();
 const express = require('express');
+const http = require('http');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const socketIo = require('socket.io');
 const { sequelize } = require('./database/data_models');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*", // Adjust to match the domain of your front end
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
 
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
 // Test DB connection
 sequelize.authenticate()
   .then(() => console.log("Connection has been established successfully."))
   .catch((error) => console.error("Unable to connect to the database:", error));
 
-// Home route for sanity check
 app.get('/', (req, res) => {
   res.send('SkyConnect Backend is running.');
 });
 
-// Start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
