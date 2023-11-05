ea9a4a7bd0c2310ca10a318e5a665fb89444f03e
Santosh Lamichhane <sant43210@gmail.com>
2023-11-04 22:34:56 -0400

    Updates on frontend , UI/UX design

--- skyconnect/src/SignUp.jsx
+++ skyconnect/src/SignUp.jsx
@@ -1,79 +1,72 @@
-import {Link, useNavigate} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './SignUp.css'; // Ensure this file exists and is correctly referenced
+import logo from './logo.png';
+
 export default function SignUp() {
-    
-    
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [useLinkedIn, setUseLinkedIn] = useState(false);
 
-    return (
-    <div className="main">
-      <div>
-        <div className="left">
-            <div className="intro">
-                <h1>Welcome to SkyConnect</h1>
-                <p>
-                SkyConnect is a platform where you can connect with people and
-                exchange ideas.
-                </p>
-            </div>
-        <div>
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
-          </div>
-        </div>
-        <div className="right">
-          <div className="signup-form">
-            Sign up here
-            <input
-              type="text"
-              label="First Name"
-              id="firstname-input"
-              aria-label="name input"
-              placeholder="enter first name"
-              
-                          />
-            <input
-              type="text"
-              label="Last Name"
-              id="lastname-input"
-              aria-label="name input"
-              placeholder="enter last name"
-              
-             
-            />
-            <input
-              type="text"
-              label="Email Address"
-              id="email-input"
-              aria-label="email input"
-              placeholder="enter email"
-             
-            
-            />
-            <input
-              type="password"
-              label="Create Password"
-              id="password-input"
-              aria-label="password input"
-              placeholder="enter password"
-              
-              
-            />
-            <input
-              type="password"
-              label="Confirm Password"
-              id="confirm-password"
-              aria-label="confirm password"
-              placeholder="confirm password"
-              
-              
-            />
-            <button className="register-btn" >
-              Register
-            </button>
-            <p>
-              Existing user? <Link to={"/login"}>Login</Link>
-            </p>
-          </div>
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // ... handle form submission logic
+  };
+
+  const handleLinkedInClick = () => {
+    window.location.href = "https://www.linkedin.com/login";
+  };
+
+  const handleUseLinkedInChange = (event) => {
+    setUseLinkedIn(event.target.checked);
+    setEmail('');
+  };
+
+  return (
+    <div className="signup-container">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="signin-logo"/>
+      </div>
+      <div className="welcome-container">
+        <div className="welcome-message">
+          <h2>Welcome to SkyConnect</h2>
+          <p>SkyConnect is a platform where you can connect with people and exchange ideas.</p>
         </div>
       </div>
+      <div className="signup-form-container">
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <h3>Sign Up Here</h3>
+          {!useLinkedIn && (
+            <>
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} required={!useLinkedIn} disabled={useLinkedIn} />
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange} required />
+            </>
+          )}
+          <div className="linkedin-checkbox-container">
+            <input type="checkbox" id="useLinkedIn" name="useLinkedIn" checked={useLinkedIn} onChange={handleUseLinkedInChange} />
+            <label htmlFor="useLinkedIn">Sign up with LinkedIn</label>
+          </div>
+          {useLinkedIn && (
+            <div className="linkedin-section">
+              <p>Enter your LinkedIn credentials to sign up:</p>
+              <button type="button" onClick={handleLinkedInClick} className="linkedin-btn">Sign up with LinkedIn</button>
+            </div>
+          )}
+          <div className="existing-user-prompt">
+            <p>Existing user? <Link to="/login" className="login-link">Login</Link></p>
+          </div>
+        </form>
+      </div>
     </div>
-    );
+  );
 }
